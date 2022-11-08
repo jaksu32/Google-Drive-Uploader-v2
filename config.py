ea4a94193f3482bdf0f714d7b15b1102d68c159c
jaksu32 <117660988+jaksu32@users.noreply.github.com>
2022-11-08 11:32:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 DOWNLOAD_LOCATION = "./Downloads"
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5587311521:AAFcGar2idhzJr3obs2hgrFXUmWzexYaqtI")
 # BOT_TOKEN = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"  #  @aryan_vbot
-APP_ID = int(os.environ.get("API_ID", "0f0ff9b47cbd315fb9672aef08eee93c"))
-API_HASH = os.environ.get("API_HASH", "15073707")
+APP_ID = int(os.environ.get("API_ID", "15073707"))
+API_HASH = os.environ.get("API_HASH", "0f0ff9b47cbd315fb9672aef08eee93c")
 AUTH_GROUP = ""
 
 Creds_path = "creds"
